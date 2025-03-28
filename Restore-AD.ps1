0750332671369bfaae6a2721b7e27e76cfef7814
itseverettparetti <everettparetti10@gmail.com>
2025-03-27 19:28:39 -0700

    Restore-AD.ps1 v2
    need comments

--- Restore-AD.ps1
+++ Restore-AD.ps1
@@ -1,21 +1,50 @@
-#Everett Paretti  
+#Everett Paretti  012026065
+
+set-location $PSScriptRoot
+
 try {
 Get-ADOrganizationalUnit -Identity "OU=Finance,DC=consultingfirm,DC=com" | Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $false }
-catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] 
-    {write-host "Finance OU not found, creating Finance OU"}
+catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
+    {Write-error "Finance OU not found"
+     write-host " "}
+
 
 try {
-if (Get-ADOrganizationalUnit -Identity "OU=Finance,DC=consultingfirm,DC=com") 
+if (Get-ADOrganizationalUnit -Identity "OU=Finance,DC=consultingfirm,DC=com")
     {Write-Host "Finance already exists, deleting"} }
 catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
-    {write-host " "}
+    {Write-Error "Finance OU not found"
+     write-host " "}
 
 try {
-Remove-ADOrganizationalUnit -identity "OU=Finance,DC=consultingfirm,DC=com" -confirm:$false }
+Remove-ADOrganizationalUnit -identity "OU=Finance,DC=consultingfirm,DC=com" -confirm:$false -Recursive }
 catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
-    {write-host " "}
+    {Write-Error "Could not delete, no finance OU found"
+     write-host " "}
 
 New-ADOrganizationalUnit -name "Finance"
 Write-Host "Finance OU created"
 
+Write-Host " "
+
+Write-Host "Creating Users"
+
+$CSVusers = Import-CSV -path .\financePersonnel.csv
+
+foreach ($User in $CSVusers)
+{
+$firstName= $User.First_Name
+$lastName= $User.Last_Name
+$displayName= "$firstName $lastName"
+$postalCode= $User.PostalCode
+$officePhone= $User.OfficePhone
+$mobilePhone= $User.MobilePhone
+$OU = "OU=Finance,DC=consultingfirm,DC=com"
+
+New-ADUser -Name $displayName -GivenName $firstName -Surname $lastName -DisplayName $displayName -PostalCode $postalCode -OfficePhone $officePhone -MobilePhone $mobilePhone -Path $OU 
+
+Get-ADUser -Filter * -SearchBase “ou=Finance,dc=consultingfirm,dc=com” -Properties DisplayName,PostalCode,OfficePhone,MobilePhone > .\AdResults.txt
+
+}
+Write-Host "Users created"
 pause

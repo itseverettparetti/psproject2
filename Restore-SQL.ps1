cb094162ce8437ca07f1793f996eee0995a8829f
itseverettparetti <everettparetti10@gmail.com>
2025-03-27 20:40:31 -0700

    Restore-SQL.ps1

--- Restore-SQL.ps1
+++ Restore-SQL.ps1
@@ -1,5 +1,23 @@
-#Everett Paretti  012026065
+#Everett Paretti 012026065
 
-set-location $PSScriptRoot
+$database = "ClientDB"
+
+try {
+$SQLdatabase = SqlServer\Get-SqlDatabase -ServerInstance SRV19-PRIMARY\SQLEXPRESS | Where-Object { $_.Name -eq $database }
+
+if ($SQLdatabase) 
+{
+Write-Output "Found database: $($SQLdatabase.Name)"
+} else {
+    Write-Output "Database '$database' not found."
+    }
+    }
+catch [Microsoft.SqlServer.Management.PowerShell.IaaS.GetSqlDatabaseCommand]
+    {Write-error "Database ClientDB Not Found"
+     write-host " "}
 
 
+
+
+
+pause
